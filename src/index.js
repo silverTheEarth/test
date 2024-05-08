51195eba26585cce223f325b88bd86301e0449c6
listValues <1528249800@qq.com>
2024-05-08 13:55:55 +0800

    init2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // index.js
 import styles from "./styles.css";
 // console.log
-console.log("styles1", styles);
+console.log("styles12", styles);
 document.getElementById("app").innerHTML = `
   <div class="${styles.container}">
     <h1>Hello, CSS Modules!</h1>
